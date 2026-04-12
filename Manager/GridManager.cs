ff007adc73d71d14f0d4e5a0190bc2d28d1be553
Mosu <M0SUM0SU@proton.me>
2026-04-12 12:30:02 +0200

    added a new component

--- Manager/GridManager.cs
+++ Manager/GridManager.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using Game.Component;
 using Godot;
 
 namespace Game.Manager;
@@ -28,9 +30,34 @@ public partial class GridManager : Node
 		occupiedCells.Add(tilesPosition);
 	}
 
-	public void HighlightValidTilesInRadius(Vector2I rootCell, int radius)
+	public void HighlightBuildableTiles()
 	{
 		ClearHighlightedTiles();
+		var buildingComponents = GetTree().GetNodesInGroup(nameof(BuildingComponent)).Cast<BuildingComponent>();
+
+		foreach (var buildingComponent in buildingComponents)
+		{
+			HighlightValidTilesInRadius(buildingComponent.GetGridCellPosition(), buildingComponent.BuildableRadius);
+		}
+	}
+
+
+	public void ClearHighlightedTiles()
+	{
+		highlightTilemapLayer.Clear();
+	}
+
+	public Vector2I GetMouseGridVectorPosition()
+	{
+		var mousePosition = highlightTilemapLayer.GetGlobalMousePosition();
+		var gridPosition =  mousePosition / 64;
+		gridPosition = gridPosition.Floor();
+		return new Vector2I((int)gridPosition.X, (int)gridPosition.Y);
+	}
+
+
+	private void HighlightValidTilesInRadius(Vector2I rootCell, int radius)
+	{
 
 		for (var x = rootCell.X - radius; x <= rootCell.X + radius; x++)
 		{
@@ -42,18 +69,4 @@ public partial class GridManager : Node
 			}
 		}
 	}
-
-	public void ClearHighlightedTiles()
-	{
-		highlightTilemapLayer.Clear();
-	}
-	
-	public Vector2I GetMouseGridVectorPosition()
-	{
-		var mousePosition = highlightTilemapLayer.GetGlobalMousePosition();
-		var gridPosition =  mousePosition / 64;
-		gridPosition = gridPosition.Floor();
-		return new Vector2I((int)gridPosition.X, (int)gridPosition.Y);
-	}
-
 }
